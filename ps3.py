f5b5991a4c64f7a31ea0ced0dec2bd52133bac1d
Ryan Dennehy <rmd5947@rit.edu>
2014-11-01 16:19:34 -0400

    Updated ps3.py
    Added the ability to write output to out.csv

--- ps3.py
+++ ps3.py
@@ -14,6 +14,7 @@ from time import time
 import logging
 from operator import itemgetter
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
@@ -24,6 +25,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import KFold
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import metrics
 
 def read_csv(path):
     output = []
@@ -33,6 +36,13 @@ def read_csv(path):
             output.append(row)
     return output
 
+def write_csv(path, subjectID, imageID, questionID, QA, EM, text):
+    with open(path, "wb") as f:
+        writer = csv.writer(f,delimiter=",",dialect="excel")
+        for i in range(len(subjectID)):
+            writer.writerow([subjectID[i], imageID[i], questionID[i], QA[i], EM[i], text[i]])
+    return text
+
 def read_dir_sk(path):
     """
     Takes a path to a directory of csv data files, parses them individually,
@@ -41,6 +51,9 @@ def read_dir_sk(path):
     X = []
     y_qa = []
     y_em = []
+    sID = []
+    iID = []
+    qID = []
     for root, subdirs, files in os.walk(path):
         for f in files:
             if f.endswith(".csv"):
@@ -55,7 +68,10 @@ def read_dir_sk(path):
                     # remove brackets around words []
                     text = re.sub(r'(<|>|\[|\]|\*)', '', row[5])
                     X.append(text)
-    return X, y_qa, y_em
+                    sID.append(row[0])
+                    iID.append(row[1])
+                    qID.append(row[2])
+    return sID, iID, qID, X, y_qa, y_em
 
 def read_dir_dict(path):
     """
@@ -187,6 +203,46 @@ def report_grid_scores(grid_scores, n_top=10):
         print("Parameters: {0}".format(score.parameters))
         print("")
 
+def get_metrics(y_test_list, y_pred_list):
+    y_true = y_test_list
+    print(y_true)
+
+    y_pred = y_pred_list
+    print(y_pred)
+
+    # report the confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    print 'Confusion Matrix:\n' + str(cm)
+
+    # plot confusion matrix in color in a separate window
+    plt.matshow(cm)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+    # report the classification metrics
+    print( classification_report(y_true, y_pred) )
+
+    # compute ROC curve and area the curve
+    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
+    roc_auc = metrics.auc(fpr, tpr)
+    print("Area under the ROC curve: %f" % roc_auc)
+
+    # plot ROC curve
+    plt.clf()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
 def main(args):
     if (args.data):
         # Our features and two sets of labels
@@ -207,8 +263,13 @@ def main(args):
         best_em_clf = em_grid_search.best_estimator_
 
     elif (args.test and args.train):
-        train_X, train_y_qa, train_y_em = read_dir_sk(args.train)
-        test_X, test_y_qa, test_y_em = read_dir_sk(args.test)
+        s1, i1, q1, train_X, train_y_qa, train_y_em = read_dir_sk(args.train)
+        s2, i2, q2, test_X, test_y_qa, test_y_em = read_dir_sk(args.test)
+
+        train_y_qa, le_qa = encode_labels(train_y_qa)
+        train_y_em, le_em = encode_labels(train_y_em)
+        test_y_qa, le_qa = encode_labels(test_y_qa)
+        test_y_em, le_em = encode_labels(test_y_em)
 
         # how many documents are in the training set?
         len_img_train = int(float(len(train_y_qa))/40.0)
@@ -226,7 +287,8 @@ def main(args):
         qa_predictions = best_qa_clf.predict(test_X)
         print(qa_predictions)
 
-        print
+        get_metrics(test_y_qa, qa_predictions)
+
         print("--- E/M ---")
         em_grid_search, em_pipe, em_params = tfidf_mnb_pipeline(train_X,
                                                                 train_y_em,
@@ -238,6 +300,9 @@ def main(args):
             best_em_clf.score(test_X, test_y_em)))
         em_predictions = best_em_clf.predict(test_X)
         print(em_predictions)
+
+        write_csv("out.csv", s2, i2, q2, qa_predictions, em_predictions, test_X)
+        get_metrics(test_y_em, em_predictions)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
