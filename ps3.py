e6ad73c609c7df45bb34b8f843eeeba1a2c309bc
kobe2452 <tongliu0314@gmail.com>
2014-11-02 15:34:24 -0500

    add POS tagger function

--- ps3.py
+++ ps3.py
@@ -3,7 +3,7 @@ Team Challenge: Predicting Turn Types
 Authors: Tong, Will, and Ryan
 """
 
-import os, re, sys, csv, argparse, logging
+import os, re, sys, csv, argparse, logging, nltk
 from pprint import pprint
 from time import time
 
@@ -91,6 +91,16 @@ def q_features(data):
 
     print("Total: ", totalCount, "Q: ", qCount, "A: ", aCount)
     print("Ratio: ", (qCount + aCount)/totalCount)
+
+def POS_feature(X):
+    for text in X:
+        token = nltk.word_tokenize(text)
+        pos_tag = nltk.pos_tag(token)
+        for item in pos_tag:
+            word = item[0]
+            pos = item[1]
+
+
 
 def encode_labels(y):
     """
@@ -244,102 +254,103 @@ def get_metrics(baseline_prob, y_test_list, y_pred_list, plot_results=True):
 def main(args):
     if (args.data):
         # Our features and two sets of labels
-        X, y_qa, y_em = read_dir_sk(args.data)
+        s1, i1, q1, train_X, train_y_qa, train_y_em, _ = read_dir_sk(args.data)
+        POS_feature(train_X)
 
-        y_qa, le_qa = encode_labels(y_qa)
-        y_em, le_em = encode_labels(y_em)
+        # y_qa, le_qa = encode_labels(y_qa)
+        # y_em, le_em = encode_labels(y_em)
 
-        print("--- Q/A ---")
-        qa_grid_search, qa_pipe, qa_params = tfidf_mnb_pipeline(X, y_qa)
-        report_grid_search(qa_grid_search, qa_pipe, qa_params)
-        best_qa_clf = qa_grid_search.best_estimator_
-
-        print
-        print("--- E/M ---")
-        em_grid_search, em_pipe, em_params = tfidf_mnb_pipeline(X, y_em)
-        report_grid_search(em_grid_search, em_pipe, em_params)
-        best_em_clf = em_grid_search.best_estimator_
-
-    elif (args.test and args.train):
-        s1, i1, q1, train_X, train_y_qa, train_y_em, _ = read_dir_sk(args.train)
-        s2, i2, q2, test_X, test_y_qa, test_y_em, test_f_names = read_dir_sk(args.test)
-
-        print(test_f_names)
-
-        train_y_qa, le_qa = encode_labels(train_y_qa)
-        train_y_em, le_em = encode_labels(train_y_em)
-        test_y_qa, le_qa = encode_labels(test_y_qa)
-        test_y_em, le_em = encode_labels(test_y_em)
-
-        train_y_qa, le_qa = encode_labels(train_y_qa)
-        train_y_em, le_em = encode_labels(train_y_em)
-        test_y_qa, le_qa = encode_labels(test_y_qa)
-        test_y_em, le_em = encode_labels(test_y_em)
-
-        em_baseline_prob = find_baseline(train_y_em)
-        qa_baseline_prob = find_baseline(train_y_qa)
-        print("Q/A baseline {0}".format(qa_baseline_prob))
-        print("E/M baseline {0}".format(em_baseline_prob))
-
-        # how many documents are in the training set?
-        len_img_train = int(float(len(train_y_qa))/float(len_img))
-        len_img_test = int(float(len(test_y_qa))/float(len_img))
-        print(len_img_train, len_img_test)
-
-        print("--- Q/A ---")
-        qa_grid_search, qa_pipe, qa_params = tfidf_mnb_pipeline(train_X,
-                                                                train_y_qa,
-                                                                len_img_train)
-        report_grid_search(qa_grid_search, qa_pipe, qa_params)
-        best_qa_clf = qa_grid_search.best_estimator_
-
-        print("Q/A performance on the left out dataset: {0}".format(
-            best_qa_clf.score(test_X, test_y_qa)))
-        qa_predictions = best_qa_clf.predict(test_X)
-        qa_prob_predictions = best_qa_clf.predict_proba(test_X)
-        print("QA prediction arrays:")
-        print(qa_predictions)
-        print(qa_prob_predictions)
-        print
-
-        print("Metrics for Q/A task on Image 1")
-        get_metrics(qa_baseline_prob, test_y_qa[:40], qa_predictions[:40], False)
-        print("Metrics for Q/A task on Image 2")
-        get_metrics(qa_baseline_prob, test_y_qa[40:], qa_predictions[40:], False)
-
-        print("--- E/M ---")
-        em_grid_search, em_pipe, em_params = tfidf_mnb_pipeline(train_X,
-                                                                train_y_em,
-                                                                len_img_train)
-        report_grid_search(em_grid_search, em_pipe, em_params)
-        best_em_clf = em_grid_search.best_estimator_
-
-        print("E/M performance on the left out dataset: {0}".format(
-            best_em_clf.score(test_X, test_y_em)))
-        em_predictions = best_em_clf.predict(test_X)
-        em_prob_predictions = best_em_clf.predict_proba(test_X)
-        print("EM prediction arrays:")
-        print(em_predictions)
-        print(em_prob_predictions)
-        print
-
-        print("Metrics for E/M task on Image 1")
-        get_metrics(em_baseline_prob, test_y_em[:40], em_predictions[:40], False)
-        print("Metrics for E/M task on Image 2")
-        get_metrics(em_baseline_prob, test_y_em[40:], em_predictions[40:], False)
-
-        s_i = 0
-        for i, f in enumerate(test_f_names):
-            e_i = (i+1) * 40
-            write_csv(f, s2[s_i:e_i],
-                         i2[s_i:e_i],
-                         q2[s_i:e_i],
-                         qa_predictions[s_i:e_i],
-                         em_predictions[s_i:e_i],
-                         test_X[s_i:e_i])
-            print("length check")
-            print(s_i, e_i)
-            s_i = e_i
+    #     print("--- Q/A ---")
+    #     qa_grid_search, qa_pipe, qa_params = tfidf_mnb_pipeline(X, y_qa)
+    #     report_grid_search(qa_grid_search, qa_pipe, qa_params)
+    #     best_qa_clf = qa_grid_search.best_estimator_
+    #
+    #     print
+    #     print("--- E/M ---")
+    #     em_grid_search, em_pipe, em_params = tfidf_mnb_pipeline(X, y_em)
+    #     report_grid_search(em_grid_search, em_pipe, em_params)
+    #     best_em_clf = em_grid_search.best_estimator_
+    #
+    # elif (args.test and args.train):
+    #     s1, i1, q1, train_X, train_y_qa, train_y_em, _ = read_dir_sk(args.train)
+    #     s2, i2, q2, test_X, test_y_qa, test_y_em, test_f_names = read_dir_sk(args.test)
+    #
+    #     print(test_f_names)
+    #
+    #     train_y_qa, le_qa = encode_labels(train_y_qa)
+    #     train_y_em, le_em = encode_labels(train_y_em)
+    #     test_y_qa, le_qa = encode_labels(test_y_qa)
+    #     test_y_em, le_em = encode_labels(test_y_em)
+    #
+    #     train_y_qa, le_qa = encode_labels(train_y_qa)
+    #     train_y_em, le_em = encode_labels(train_y_em)
+    #     test_y_qa, le_qa = encode_labels(test_y_qa)
+    #     test_y_em, le_em = encode_labels(test_y_em)
+    #
+    #     em_baseline_prob = find_baseline(train_y_em)
+    #     qa_baseline_prob = find_baseline(train_y_qa)
+    #     print("Q/A baseline {0}".format(qa_baseline_prob))
+    #     print("E/M baseline {0}".format(em_baseline_prob))
+    #
+    #     # how many documents are in the training set?
+    #     len_img_train = int(float(len(train_y_qa))/float(len_img))
+    #     len_img_test = int(float(len(test_y_qa))/float(len_img))
+    #     print(len_img_train, len_img_test)
+    #
+    #     print("--- Q/A ---")
+    #     qa_grid_search, qa_pipe, qa_params = tfidf_mnb_pipeline(train_X,
+    #                                                             train_y_qa,
+    #                                                             len_img_train)
+    #     report_grid_search(qa_grid_search, qa_pipe, qa_params)
+    #     best_qa_clf = qa_grid_search.best_estimator_
+    #
+    #     print("Q/A performance on the left out dataset: {0}".format(
+    #         best_qa_clf.score(test_X, test_y_qa)))
+    #     qa_predictions = best_qa_clf.predict(test_X)
+    #     qa_prob_predictions = best_qa_clf.predict_proba(test_X)
+    #     print("QA prediction arrays:")
+    #     print(qa_predictions)
+    #     print(qa_prob_predictions)
+    #     print
+    #
+    #     print("Metrics for Q/A task on Image 1")
+    #     get_metrics(qa_baseline_prob, test_y_qa[:40], qa_predictions[:40], False)
+    #     print("Metrics for Q/A task on Image 2")
+    #     get_metrics(qa_baseline_prob, test_y_qa[40:], qa_predictions[40:], False)
+    #
+    #     print("--- E/M ---")
+    #     em_grid_search, em_pipe, em_params = tfidf_mnb_pipeline(train_X,
+    #                                                             train_y_em,
+    #                                                             len_img_train)
+    #     report_grid_search(em_grid_search, em_pipe, em_params)
+    #     best_em_clf = em_grid_search.best_estimator_
+    #
+    #     print("E/M performance on the left out dataset: {0}".format(
+    #         best_em_clf.score(test_X, test_y_em)))
+    #     em_predictions = best_em_clf.predict(test_X)
+    #     em_prob_predictions = best_em_clf.predict_proba(test_X)
+    #     print("EM prediction arrays:")
+    #     print(em_predictions)
+    #     print(em_prob_predictions)
+    #     print
+    #
+    #     print("Metrics for E/M task on Image 1")
+    #     get_metrics(em_baseline_prob, test_y_em[:40], em_predictions[:40], False)
+    #     print("Metrics for E/M task on Image 2")
+    #     get_metrics(em_baseline_prob, test_y_em[40:], em_predictions[40:], False)
+    #
+    #     s_i = 0
+    #     for i, f in enumerate(test_f_names):
+    #         e_i = (i+1) * 40
+    #         write_csv(f, s2[s_i:e_i],
+    #                      i2[s_i:e_i],
+    #                      q2[s_i:e_i],
+    #                      qa_predictions[s_i:e_i],
+    #                      em_predictions[s_i:e_i],
+    #                      test_X[s_i:e_i])
+    #         print("length check")
+    #         print(s_i, e_i)
+    #         s_i = e_i
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
